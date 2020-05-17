52bc2add64e855b1250eb50309464445c8d417ee
Dominic Brant <dombrant@gmail.com>
2020-05-17 11:38:51 -0700

    added device dimensions

---
+++ src/js/devices.js
@@ -0,0 +1,12 @@
+const devices = {
+  iPhone11ProMax: { width: 1242, height: 2688 },
+  iPhone11Pro: { width: 1125, height: 2436 },
+  iPhone11: { width: 828, height: 1792 },
+  iPhoneSE: { width: 750, height: 1334 },
+  GooglePixel4XL: { width: 1440, height: 3040 },
+  GooglePixel4: { width: 1080, height: 2280 },
+  SamsungGalaxyS20: { width: 1440, height: 3200 },
+  MotoG8: { width: 720, height: 1560 },
+};
+
+export default devices;
