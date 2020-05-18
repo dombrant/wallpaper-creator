14c53d3ee6e810f27740abbf521bb54716669bdd
Dominic Brant <dombrant@gmail.com>
2020-05-18 08:39:54 -0700

    changed formatting of device names

--- src/js/devices.js
+++ src/js/devices.js
@@ -3,10 +3,10 @@ const devices = {
   iPhone11Pro: { width: 1125, height: 2436 },
   iPhone11: { width: 828, height: 1792 },
   iPhoneSE: { width: 750, height: 1334 },
-  GooglePixel4XL: { width: 1440, height: 3040 },
-  GooglePixel4: { width: 1080, height: 2280 },
-  SamsungGalaxyS20: { width: 1440, height: 3200 },
-  MotoG8: { width: 720, height: 1560 },
+  googlePixel4XL: { width: 1440, height: 3040 },
+  googlePixel4: { width: 1080, height: 2280 },
+  samsungGalaxyS20: { width: 1440, height: 3200 },
+  motoG8: { width: 720, height: 1560 },
 };
 
 export default devices;
